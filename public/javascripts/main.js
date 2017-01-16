94c4641a5a2c18e6fea1bfcb68ac66f8f04d41f6
Delmer <delmer814+1@gmail.com>
2017-01-16 16:24:57 -0500

    Add problem descriptions and tests
    Adds a set of restaurant related exercises with tests.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,51 @@
 /*
   Function Exercises
 */
+
+// 1. Write a function called 'add' that has two Number params and adds them together.
+
+// 2. Write a function called 'ableToDrink' that has an 'age' param and
+//  returns false if age is less than 21 and true otherwise.
+
+// 3. Write a function called 'nextCustomer' that has a 'waitingCustomers' param
+// that is an array of customer names. The function should return the first
+// name in the list.
+
+// 4. Write a function called 'addCustomer' that has two params 'waitingCustomers',
+//  an array of names, and 'newCustomer', a name of a new customer.
+//
+//  The 'addCustomer' function should add the 'newCustomer' to the end of
+//  'waitingCustomers'. It doesn't have a return.
+
+
+// 5. Write a function called 'seatCustomer' with a 'waitingCustomers' param and that
+//  removes first customer name and returns it.
+
+// 6. Write a function called 'waitPerPerson' with a 'seatedCount' param.
+//
+//  Assume the following rules for waitPerPerson
+//
+//  1. For less than five seated people there is a 2 minute wait per person.
+//  2. For five or more seated people the seating rate is 4 minutes per person.
+//  3. For ten or more seated people the seating rate 6 minutes per person.
+//  4. For fifteen or more seated people the seating rate is 10 minutes per person.
+//
+
+// 7. Write a function called 'waitTime' with two params: 'seatedCount', a number, and
+// 'waitingCount', the number of waiting customers. The 'waitTime' should compute
+// the total wait for a new customer using the 'waitPerPerson' and 'waitingCount'.
+//
+
+// 8. Write a function called 'customersBefore' with both a 'waitingCustomers' and 'name'
+//  param. It should use the 'name' to count the number of customers before that
+//  name in customers list
+//
+
+// 9. Write a function called 'hasReservation' with a 'reservations' param, an
+//  array of names, and a 'name' param. The function should return true or false
+//  if the name is on the list.
+
+
+// 10.) Write a function called 'timeRemaining' with three params: 'waitingCustomers',
+// a 'name', and 'seatedCount'. Count the number of customers before the given 'name'
+// and use the 'seatedCount' to determine the 'waitTime'.
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,8 +1,241 @@
 var expect = chai.expect;    // Using Expect style
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      expect([1,2,3].indexOf(4)).to.equal(-1);
+
+describe('Problem 1', function () {
+  it('should define an add function', function () {
+    expect(function () {
+      add();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('add', function () {
+    it('should add two numbers', function () {
+      expect(add(2,3)).to.equal(6);
+    });
+  });
+});
+
+describe('Problem 2', function() {
+  it('should define an allowedToDrink function', function () {
+    expect(function () {
+      allowedToDrink();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('allowedToDrink', function() {
+    it('should return true when age is 21', function() {
+      expect(allowedToDrink(21)).to.equal(true)
+    });
+
+    it('should return true when age is over 21', function() {
+      expect(allowedToDrink(22)).to.equal(true)
+    });
+
+    it('should return false when age is less 21', function() {
+      expect(allowedToDrink(20)).to.equal(false)
+    });
+  });
+});
+
+describe('Problem 3', function () {
+  it('should define an nextCustomer function', function () {
+    expect(function () {
+      nextCustomer();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('nextCustomer', function () {
+    it('should return undefined if no customers', function () {
+      expect(nextCustomer([])).to.equal(undefined);
+    });
+
+    it('should return the first customer', function () {
+      expect(nextCustomer(['john', 'jane', 'bob'])).to.equal('john');
+    });
+
+    it('should not change the customers', function () {
+      var waiting = ['john', 'jane', 'bob'];
+
+      expect(nextCustomer(['john', 'jane', 'bob'])).to.equal('john');
+      expect(waiting).to.equal(['john', 'jane', 'bob']);
+    })
+  });
+});
+
+describe('Problem 4', function () {
+  it('should define an addCustomer function', function () {
+    expect(function () {
+      addCustomer();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('addCustomer', function () {
+    it('should add a customer to an empty waiting list', function () {
+      var waiting = [];
+      addCustomer(waiting, 'john');
+
+      expect(waiting).to.be.eql(['john']);
+    });
+  });
+
+  describe('addCustomer', function () {
+    it('should add a customer to the end of a wait list', function () {
+      var waiting = ['john', 'jane'];
+
+      addCustomer(waiting, 'bob');
+
+      expect(waiting).to.be.eql(['john', 'jane', 'bob']);
+    });
+  });
+});
+
+
+describe('Problem 5', function () {
+  it('should define an seatCustomer function', function () {
+    expect(function () {
+      seatCustomer();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('seatCustomer', function () {
+    it('should seatCustomer the customer', function () {
+      var waiting = ['john', 'jane', 'bob'];
+
+      expect(seatCustomer(waiting)).to.equal('john');
+      expect(waiting).to.be.eql(['jane', 'bob'])
+    });
+  });
+});
+
+describe('Problem 6', function () {
+  it('should define an waitPerPerson function', function () {
+    expect(function () {
+      waitPerPerson();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('waitPerPerson', function () {
+    it('should be two minutes when less than 5 are people seated', function () {
+      expect(waitPerPerson(4)).to.equal(2);
+    });
+
+    it('should be four minutes when 5 to 9 are people seated', function () {
+      expect(waitPerPerson(5)).to.equal(4);
+      expect(waitPerPerson(9)).to.equal(4);
+    });
+
+    it('should be six minutes when 10 to 14 are people seated', function () {
+      expect(waitPerPerson(10)).to.equal(6);
+      expect(waitPerPerson(14)).to.equal(6);
+    });
+
+    it('should be 10 minutes when 15 or more  people seated', function () {
+      expect(waitPerPerson(15)).to.equal(10);
+      expect(waitPerPerson(16)).to.equal(10);
+    });
+  });
+});
+
+
+describe('Problem 7', function () {
+  it('should define an waitTime function', function () {
+    expect(function () {
+      waitTime();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('waitTime', function () {
+    it('should take 16 minutes when 4 seated people and 8 waiting', function () {
+      expect(waitTime(4, 8)).to.equal(16);
+    });
+
+    it('should take 32 minutes when 5 seated people and 8 waiting', function () {
+      expect(waitTime(5, 8)).to.equal(32);
+    });
+
+    it('should take 48 minutes when 10 seated people and 8 waiting', function () {
+      expect(waitTime(10, 8)).to.equal(48);
+    });
+
+    it('should take 80 minutes when 15 seated people and 8 waiting', function () {
+      expect(waitTime(15, 8)).to.equal(80);
+    });
+  });
+});
+
+
+describe('Problem 8', function () {
+  it('should define an customersBefore function', function () {
+    expect(function () {
+      customersBefore();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('customersBefore', function () {
+    it('should count the names before the provided name', function () {
+      var waitingCustomers = ['john', 'jane', 'bob', 'danny'];
+
+      expect(customersBefore(waitingCustomers, 'bob')).to.equal(2)
+      expect(customersBefore(waitingCustomers, 'john')).to.equal(0)
+    });
+
+    it('should return undefined if the name is not a waiting customer', function () {
+      var waitingCustomers = ['john', 'jane', 'bob', 'danny'];
+
+      expect(customersBefore(waitingCustomers, 'jack')).to.equal(undefined)
+    });
+
+    it('should return undefined if no one is waiting', function () {
+      var waitingCustomers = [];
+
+      expect(customersBefore(waitingCustomers, 'jack')).to.equal(undefined)
+    });
+  });
+});
+
+describe('Problem 9', function () {
+  it('should define an hasReservation function', function () {
+    expect(function () {
+      hasReservation();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('hasReservation', function () {
+    it('should return true when on reservations list', function () {
+      var reservations = ['sam', 'taylor', 'ruby'];
+
+      expect(hasReservation(reservations, 'taylor')).to.equal(true);
+    });
+
+    it('should return false not on reservations list', function () {
+      var reservations = ['sam', 'taylor', 'ruby'];
+
+      expect(hasReservation(reservations, 'lisa')).to.equal(false);
+    });
+  });
+});
+
+describe('Problem 10', function () {
+  it('should define an timeRemaining function', function () {
+    expect(function () {
+      timeRemaining();
+    }).to.not.throw(ReferenceError);
+  });
+
+  describe('timeRemaining', function () {
+    it('should return the expected wait for given person', function () {
+      var waiting     = ['john', 'jane', 'jack', 'ruby'];
+      var name        = 'jack';
+
+      expect(timeRemaining(waiting, name, 4)).to.equal(4);
+      expect(timeRemaining(waiting, name, 10)).to.equal(12);
+    });
+
+    it('should return total wait time (like problem 7) if not waiting', function () {
+      var waiting     = ['john', 'jane', 'jack', 'ruby'];
+      var name        = 'bob';
+
+      expect(timeRemaining(waiting, name, 4)).to.equal(8);
+      expect(timeRemaining(waiting, name, 10)).to.equal(24);
     });
   });
 });
