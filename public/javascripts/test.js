b75be96f0386f1934039c7f8558896b3a3902337
Delmer <delmer814+1@gmail.com>
2017-01-16 13:10:33 -0500

    Add tests and folders
    Adds folders and libs for testing our exercises.

---
+++ public/javascripts/test.js
@@ -0,0 +1,8 @@
+var expect = chai.expect;    // Using Expect style
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect([1,2,3].indexOf(4)).to.equal(-1);
+    });
+  });
+});
